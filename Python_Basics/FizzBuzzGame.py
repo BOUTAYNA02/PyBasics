20125f437ef512cb84581e07bac86462786e32b8
BOUTAYNA02 <zakariboutayna02@gmail.com>
2024-11-30 17:14:16 +0100

    1st commit

--- Python_Basics/FizzBuzzGame.py
+++ Python_Basics/FizzBuzzGame.py
@@ -1,10 +1,13 @@
-n = int(input("Enter a number: "))
-for i in range(1, n + 1):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    elif True:
-        print(i)
+n = int(input("Enter a number : "))
+while n != 0:
+    for i in range(1, n + 1):
+        if int(i / 3) * 3 == i and int(i / 5) * 5 == i:
+            print("FizzBuzz")
+        elif int(i / 3) * 3 == i:
+            print("Fizz")
+        elif int(i / 5) * 5 == i:
+            print("Buzz")
+        else:
+            print(i)
+    n = int(input("Enter a number : "))
+
--- Python_Basics/leapyear.py
+++ Python_Basics/leapyear.py
@@ -1,11 +1,10 @@
 for i in range(1, 1000):
     year = int(input("Enter a year: "))
-
-    if year % 400 == 0:
+    if (year / 400) * 400 == year:
         print(f"{year} is a leap year")
-    elif year % 100 == 0:
+    elif (year / 100) * 100 == year:
         print(f"{year} is not a leap year")
-    elif year % 4 == 0:
+    elif (year / 4) * 4 == year:
         print(f"{year} is a leap year")
     else:
         print(f"{year} is not a leap year")
